0d311142cdd8dbb2c0b5a71c91ade6d73969f38c
Adam Meza <Adammeza95@gmail.com>
2023-08-03 09:30:38 -0400

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Header.css'
 import { NavLink } from 'react-router-dom'
+import titleImage from '../../assets/title.png'
 
 export const Header = () => {
   return (
     <header className='header'>
-      <NavLink to ='/' className='nav'><h1>Adam York Times</h1></NavLink>
+      <NavLink to ='/' className='nav'>
+        <img className='title-image' src={titleImage}/>
+      </NavLink>
       <div className="sort-button-container">
       <NavLink to ='/search' className='nav'><button className='sort-button'>SEARCH</button></NavLink>
         {/* <button className='sort-button'>ABC</button>
